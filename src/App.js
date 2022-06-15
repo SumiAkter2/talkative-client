187ad9d529434cccceeed15b58384627d27e637b
SumiAkter2 <akteralisha36@gmail.com>
2022-06-15 12:57:44 +0600

    added chatting nav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import MessagePage from './Pages/MessagePage';
+import Chatting from './Pages/Chatting';
 
 function App() {
   return (
-    <div>
+    <div class=" grid lg:grid-cols-3  gap-4">
+      <div>  <MessagePage /></div>
+      <div> <Chatting /></div>
+      <div> <h2>hi</h2></div>
 
-      <MessagePage />
     </div>
   );
 }
